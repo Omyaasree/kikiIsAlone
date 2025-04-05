421d9018cbae23c3645d5a1de3b793b057a5a938
Omyaasree Balaji <osbalaji@dons.usfca.edu>
2025-04-05 10:26:34 -0700

    commit change te the paje js for the on click button

--- kikis-connect/generate_qr.py
+++ kikis-connect/generate_qr.py
@@ -4,7 +4,7 @@ import segno
 
 # Generate QR with a Kiki-themed message
 
-url = "https://kikisconnect-pi.vercel.app/"#"https://stackoverflow.com/questions/881092/how-to-merge-a-specific-commit-in-git"
+url ="https://kikiisalone.vercel.app/"# "https://kikisconnect-pi.vercel.app/"#"https://stackoverflow.com/questions/881092/how-to-merge-a-specific-commit-in-git"
 qr_kiki = segno.make_qr(url)
 
 
